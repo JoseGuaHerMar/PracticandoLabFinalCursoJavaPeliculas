a8dfafb03a87e33fa4ee58eb4711f9cce3597ef8
Jose Guadalupe <joseguadalupe.1997@hotmail.com>
2021-07-10 22:05:26 -0500

    Agregado la clase Pelicula

--- src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
+++ src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cpjlaboratoriofinal;
 
-/**
- *
- * @author Omen
- */
 public class CPJLaboratorioFinal {
-    
+
+    public static void main(String[] args) {
+
+    }
+
 }
