6fdeed759e8d6701a15722e88ac10ae13c5f3cfd
Jose Guadalupe <joseguadalupe.1997@hotmail.com>
2021-07-11 21:13:34 -0500

    Agregado basico en clase Main

--- src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
+++ src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
@@ -1,9 +1,16 @@
 package cpjlaboratoriofinal;
 
+import java.util.Scanner;
+
 public class CPJLaboratorioFinal {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner (System.in);
+        int opcion;
+        String nombreArchivo;
+        
+        
     }
 
 }
