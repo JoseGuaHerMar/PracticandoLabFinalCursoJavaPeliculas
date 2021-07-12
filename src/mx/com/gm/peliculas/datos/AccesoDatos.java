d29878bc9dc300460e225c1795f5aa6b77eac50c
Jose Guadalupe <joseguadalupe.1997@hotmail.com>
2021-07-11 21:05:39 -0500

    Agregadas las clases faltantes

---
+++ src/mx/com/gm/peliculas/datos/AccesoDatos.java
@@ -0,0 +1,11 @@
+package mx.com.gm.peliculas.datos;
+
+public class AccesoDatos {
+    
+
+            
+    
+    
+    
+    
+}
---
+++ src/mx/com/gm/peliculas/negocio/CatalogoPeliculasImpl.java
@@ -0,0 +1,30 @@
+package mx.com.gm.peliculas.negocio;
+
+import mx.com.gm.peliculas.datos.AccesoDatos;
+
+public class CatalogoPeliculasImpl {
+
+    AccesoDatos datos = new AccesoDatos();
+    
+    public CatalogoPeliculasImpl(){
+        
+    }
+    
+    public void agregarPelicula(String NombrePel, String NombreArchivo){
+        
+    }
+    
+    public void listarPeliculas(String NombreArchivo){
+        
+    }
+    
+    public void buscarPelicula (String buscar, String NombreArchivo){
+        
+    }
+    
+    public void iniciarArchivo (String NombreArchivo){
+        
+    }
+    
+    
+}
